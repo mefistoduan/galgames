27efcf6c3df26a1e5d801e8f6967ea16c5a485b4
Changpeng Duan <mefistoduan@users.noreply.github.com>
2020-03-30 23:04:20 +0800

    basic

--- src/story/index.js
+++ src/story/index.js
@@ -1,103 +1,82 @@
-// author: 八方龙杀阵
-
-var datas = [
-    {
-        id : 1,
-        role : {id : 1},
-        content : '公元 768 年法兰克王国边境',
-        next : {id : 2},
-        backgroundImage : 'bg01.jpg'
-    },
-    {
-        id : 2,
-        role : {id : 1},
-        content : '这里是第二句话',
-        next : {id : 3},
-        backgroundImage : 'bg01.jpg'
-    },
-    {
-        id : 3,
-        role : {id : 2},
-        content : '这里是第三句话',
-        backgroundImage : 'bg01.jpg',
-        next : {
-            choice : [
-                {text : '回到第一句', id : 1},
-                {text : '游戏结束', id : 4}
-            ]
-        }
-    },
-    {
-        id : 4,
-        content : '游戏结束',
-        backgroundImage : 'office2.jpg'
-    }
-];
-var roles = [
-    {
-        id : 0,
-        name : '旁白',
-        karma : 0,//因果，业障
-        friend : 0,//友好值
-        health : 100,//健康
-        god : 0,//神性
-        human : 0,//人性
-        devil : 0,//魔性
-    },
-    {
-        id : 1,
-        name : '阿季卢尔福',
-        karma : 0,//因果，业障
-        friend : 100,//友好值
-        health : 100,//健康
-        god : 100,//神性
-        human : 1,//人性
-        devil : 0,//魔性
-    },
-    {
-        id : 2,
-        name : '查里曼大帝',
-        karma : 0,//因果，业障
-        friend : 0,//友好值
-        health : 80,//健康
-        god : 0,//神性
-        human : 1,//人性
-        devil : 0,//魔性
-    },
+let roles = [
+  {
+    id: 0,
+    name: '旁白',
+    karma: 0,//因果，业障
+    friend: 0,//友好值
+    health: 100,//健康
+    god: 0,//神性
+    human: 0,//人性
+    devil: 0,//魔性
+  },
+  {
+    id: 1,
+    name: '阿季卢尔福',
+    karma: 0,//因果，业障
+    friend: 100,//友好值
+    health: 100,//健康
+    god: 100,//神性
+    human: 1,//人性
+    devil: 0,//魔性
+  },
+  {
+    id: 2,
+    name: '查里曼大帝',
+    karma: 0,//因果，业障
+    friend: 0,//友好值
+    health: 80,//健康
+    god: 0,//神性
+    human: 1,//人性
+    devil: 0,//魔性
+  },
+  {
+    id: 3,
+    name: '骑士甲',
+    karma: 0,//因果，业障
+    friend: 0,//友好值
+    health: 80,//健康
+    god: 0,//神性
+    human: 1,//人性
+    devil: 0,//魔性
+  },
 ];
 
-//var speaker_1 = {
-//	name:'角色A',
-//	karma:0,//因果，业障
-//	friend:0,//友好值
-//	health:100,//健康
-//	god:0,//神性
-//	human:1,//人性
-//	devil:0,//魔性
-//}
-let dataList = {};
-let roleList = {};
+let events = [
+  {
+    id: 0,
+    content: [
+      {rid: 0, text: '公元 768 年法兰克王国边境'},
+      {rid: 2, text: '法兰克的 卫士，您是谁？'},
+      {rid: 3, text: '鄙人是布列塔尼的所罗门，陛下!我们有5000名骑兵，3500位步兵和1800个侍从，每一个都至少征战过5年。'},
+      {rid: 2, text: '很好！请退回布列塔尼人的队列，勇士！'},
+    ],
+    background: 'bg01.png',
+    nextEvent: 1,
+  },
+  {
+    id: 1,
+    content: [
+
+    ],
+    background: 'bg02.png',
+    nextEvent: 2,
+  },
+  {
+    id: 1,
+    content: [
+
+    ],
+    background: 'bg02.png',
+    nextEvent: 0,
+  }
+];
+
 let content = {};
 
-// 信息、角色等基础数据由数据对象维护
-function initData(){
-    // $.each(datas, function(i, item){
-    //     dataList[item.id] = item;
-    // });
-    // $.each(roles, function(i, item){
-    //     roleList[item.id] = item;
-    // });
-
-
-    // datas.forEach(function (e,i) {
-    //     dataList[e.id] = i;
-    // });
-    // roles.forEach(function (e,i) {
-    //     roleList[e.id] = i;
-    // });
-     content['datas'] = datas;
-     content['roles'] = roles;
-    return  content
+function initData() {
+  content['events'] = events;
+  content['roles'] = roles;
+  return content
 }
 
 module.exports = initData;
